83d195b211b9b10753dc553af13d23cda265fa09
Ania123678 <102367840+Ania123678@users.noreply.github.com>
2023-07-31 13:46:55 +0200

    ProductsNavbar changes

--- src/pages/main-page/ProductsNavbar.jsx
+++ src/pages/main-page/ProductsNavbar.jsx
@@ -22,18 +22,57 @@ const ProductsNavbar = () => {
 
   return (
     <nav className="navbar-category-products navbar-expand-lg">
-      <div className="container">
+      <div className="container container-category-products">
         <ul className="navbar-nav d-flex justify-content-between flex-wrap">
 
-          <li className="nav-item">
-            <div className="nav-link-title-products" id="makeup-link" onMouseEnter={() => handleMouseEnter('New products')} onMouseLeave={handleMouseLeave}>
+          <li className="nav-item" onMouseEnter={() => handleMouseEnter('New products')} onMouseLeave={handleMouseLeave}>
+            <div className="nav-link-title-products" id="makeup-link">
               <p style={{ color: 'rgb(255, 133, 81)' }}>New products</p>
             </div>
+
+            {hoveredCategory  === 'New products' && (
+              <div>
+              <div className="black-filter"></div>
+              <motion.div
+                className="category-list-container-motion"
+                initial={{ opacity: 0, x: -30 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ delay: 0.1 }}
+                onMouseEnter={() => handleMouseEnter('Makeup')}
+                onMouseLeave={handleMouseLeave}
+              >
+
+              <div className="category-list-container">
+                <div className='row'>
+
+                  <div className='col-4'> 
+                    <div className="image-wrapper-category-new-products">
+                      <img src={makeupCategory} alt="makeup-category-image" />
+                    </div>  
+                  </div>
+
+                  <div className='col-4'> 
+                    <div className="image-wrapper-category-new-products">
+                      <img src={makeupCategory} alt="makeup-category-image" />
+                    </div>  
+                  </div>
+
+                  <div className='col-4'> 
+                    <div className="image-wrapper-category-new-products">
+                      <img src={makeupCategory} alt="makeup-category-image" />
+                    </div>  
+                  </div>
+
+                </div>
+              </div>
+
+              </motion.div>
+            </div>
+            )}
+            
           </li>
 
           <li className="nav-item" onMouseEnter={() => handleMouseEnter('Makeup')} onMouseLeave={handleMouseLeave}>
-            
-            
             <div className="nav-link-title-products" id="makeup-link">
               Makeup
             </div>
@@ -103,12 +142,12 @@ const ProductsNavbar = () => {
               
           </li>
 
-          <li className="nav-item" onMouseEnter={() => handleMouseEnter('Perfumes')} onMouseLeave={handleMouseLeave}>
+          <li className="nav-item" onMouseEnter={() => handleMouseEnter('Fragrance')} onMouseLeave={handleMouseLeave}>
             <div className="nav-link-title-products" id="makeup-link">
-              Perfumes
+              Fragrance
             </div>
 
-            {hoveredCategory  === 'Perfumes' && (
+            {hoveredCategory  === 'Fragrance' && (
                 <div>
                 <div className="black-filter"></div>
                 <motion.div
@@ -120,43 +159,43 @@ const ProductsNavbar = () => {
 
                 <div className="category-list-container">
                   <Category
-                    title="Perfume 1"
+                    title="Woman"
                     subcategories={[
-                      'Foundation',
-                      'BB & CC Creams',
-                      'Concealer',
-                      'Setting Spray & Powder',
-                      'Face Sets'
+                      'Eau de Parfum',
+                      'Eau de Toilette',
+                      'Solid Perfume',
+                      'Unisex',
+                      'Perfume Oil'
                     ]}
                   />
                   <Category
-                    title="Perfume 2"
+                    title="Men"
                     subcategories={[
-                      'Eye Palettes',
-                      'Mascara',
-                      'Eyeliner',
-                      'Eyeshadow',
-                      'False Eyelashes'
+                      'Eau de Parfum',
+                      'Eau de Toilette',
+                      'Solid Perfume',
+                      'Unisex',
+                      'Perfume Oil'
                     ]}
                   />
                     <Category
-                    title="Perfume 3"
+                    title="Bestsellers"
                     subcategories={[
-                      'Lipstick',
-                      'Lip Gloss',
-                      'Lip Liner',
-                      'Lip Oil',
-                      'Lip Sets'
+                      'Perfume Oil',
+                      'Solid Perfume',
+                      'Oriental',
+                      'Floral',
+                      'Unisex'
                     ]}
                   />
                   <Category
-                    title="Perfume 4"
+                    title="Fragrance Family"
                     subcategories={[
-                      'Blush',
-                      'Bronzer',
-                      'Highlighter',
-                      'Contour',
-                      'Cheek Palettes'
+                      'Floral',
+                      'Oriental',
+                      'Woody',
+                      'Citrus',
+                      'Green'
                     ]} 
                   />
 
@@ -170,8 +209,8 @@ const ProductsNavbar = () => {
               )}
           </li>
 
-          <li className="nav-item">
-            <div className="nav-link-title-products" id="makeup-link" onMouseEnter={() => handleMouseEnter('Face')} onMouseLeave={handleMouseLeave}>
+          <li className="nav-item" onMouseEnter={() => handleMouseEnter('Face')} onMouseLeave={handleMouseLeave}>
+            <div className="nav-link-title-products" id="makeup-link">
               Face
             </div>
 
@@ -187,43 +226,43 @@ const ProductsNavbar = () => {
 
                 <div className="category-list-container">
                   <Category
-                    title="Cream 1"
+                    title="Creams"
                     subcategories={[
-                      'Foundation',
-                      'BB & CC Creams',
-                      'Concealer',
-                      'Setting Spray & Powder',
-                      'Face Sets'
+                      'Hydrating',
+                      'Anti-Aging',
+                      'Soothing',
+                      'Mattifying ',
+                      'Brightening'
                     ]}
                   />
                   <Category
                     title="Tonic"
                     subcategories={[
-                      'Eye Palettes',
-                      'Mascara',
-                      'Eyeliner',
-                      'Eyeshadow',
-                      'False Eyelashes'
+                      'Hydrating',
+                      'Exfoliating',
+                      'Soothing',
+                      'Pore-Refining ',
+                      'Hydrating Mist'
                     ]}
                   />
                     <Category
                     title="Eye cream"
                     subcategories={[
-                      'Lipstick',
-                      'Lip Gloss',
-                      'Lip Liner',
-                      'Lip Oil',
-                      'Lip Sets'
+                      'Hydrating',
+                      'Anti-Aging',
+                      'Firming',
+                      'Cooling',
+                      'Brightening'
                     ]}
                   />
                   <Category
                     title="Lip cream"
                     subcategories={[
-                      'Blush',
-                      'Bronzer',
-                      'Highlighter',
-                      'Contour',
-                      'Cheek Palettes'
+                      'Moisturizing',
+                      'Overnight',
+                      'Tinted',
+                      'Lip Mask',
+                      'Lip Salve'
                     ]} 
                   />
 
@@ -237,8 +276,8 @@ const ProductsNavbar = () => {
               )}
           </li>
 
-          <li className="nav-item">
-            <div className="nav-link-title-products" id="makeup-link" onMouseEnter={() => handleMouseEnter('Body')} onMouseLeave={handleMouseLeave}>
+          <li className="nav-item" onMouseEnter={() => handleMouseEnter('Body')} onMouseLeave={handleMouseLeave}>
+            <div className="nav-link-title-products" id="makeup-link">
               Body
             </div>
 
@@ -254,70 +293,121 @@ const ProductsNavbar = () => {
 
                 <div className="category-list-container">
                   <Category
-                    title="Body 1"
+                    title="Lotion & Moisturizers"
                     subcategories={[
-                      'Foundation',
-                      'BB & CC Creams',
-                      'Concealer',
-                      'Setting Spray & Powder',
-                      'Face Sets'
+                      'Hydrating',
+                      'Intensive',
+                      'Nourishing',
+                      'Soothing'
                     ]}
                   />
                   <Category
-                    title="Body 1"
+                    title="Wash & Shower Gels"
                     subcategories={[
-                      'Eye Palettes',
-                      'Mascara',
-                      'Eyeliner',
-                      'Eyeshadow',
-                      'False Eyelashes'
+                      'Refreshing',
+                      'Moisturizing',
+                      'Exfoliating',
+                      'Sensitive'
                     ]}
                   />
                     <Category
-                    title="Body 1"
+                    title="Scrubs & Exfoliators"
                     subcategories={[
-                      'Lipstick',
-                      'Lip Gloss',
-                      'Lip Liner',
-                      'Lip Oil',
-                      'Lip Sets'
+                      'Sugar',
+                      'Salt',
+                      'Coffee',
+                      'Fruit Enzyme'
                     ]}
                   />
                   <Category
-                    title="Body 1"
+                    title="Fragrances & Mists"
                     subcategories={[
-                      'Blush',
-                      'Bronzer',
-                      'Highlighter',
-                      'Contour',
-                      'Cheek Palettes'
+                      'Body Sprays',
+                      'Perfumed',
+                      'Solid Perfume',
+                      'Fragrance Lotions'
                     ]} 
                   />
 
                   <div className="image-wrapper-category">
                     <img src={makeupCategory} alt="makeup-category-image" />
                   </div>
-
                 </div>
                 </motion.div>
               </div>
               )}
           </li>
 
-          <li className="nav-item">
-            <div className="nav-link-title-products" id="makeup-link" onMouseEnter={() => handleMouseEnter('Hair')} onMouseLeave={handleMouseLeave}>
+          <li className="nav-item" onMouseEnter={() => handleMouseEnter('Hair')} onMouseLeave={handleMouseLeave}>
+            <div className="nav-link-title-products" id="makeup-link">
               Hair
             </div>
+
+            {hoveredCategory  === 'Hair' && (
+                <div>
+                <div className="black-filter"></div>
+                <motion.div
+                  className="category-list-container-motion"
+                  initial={{ opacity: 0, x: -30 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ delay: 0.1 }}
+                >
+
+                <div className="category-list-container">
+                  <Category
+                    title="Shampoo"
+                    subcategories={[
+                      'Moisturizing',
+                      'Clarifying',
+                      'Color-Protecting',
+                      'Volumizing'
+                    ]}
+                  />
+                  <Category
+                    title="Conditioner"
+                    subcategories={[
+                      'Repairing',
+                      'Hydrating',
+                      'Leave-In',
+                      'Color-Protecting'
+                    ]}
+                  />
+                    <Category
+                    title="Hair Styling"
+                    subcategories={[
+                      'Hair Spray',
+                      'Hair Gel',
+                      'Hair Mousse',
+                      'Hair Serum'
+                    ]}
+                  />
+                  <Category
+                    title="Treatments"
+                    subcategories={[
+                      'Deep Conditioning Mask',
+                      'Scalp Treatments',
+                      'Hair Oil',
+                      'Protein Treatments'
+                    ]} 
+                  />
+
+                  <div className="image-wrapper-category">
+                    <img src={makeupCategory} alt="makeup-category-image" />
+                  </div>
+                </div>
+                </motion.div>
+              </div>
+              )}
           </li>
 
-          <li className="nav-item">
-            <div className="nav-link-title-products" id="makeup-link" onMouseEnter={() => handleMouseEnter('Sale')} onMouseLeave={handleMouseLeave}>
+          <li className="nav-item" onMouseEnter={() => handleMouseEnter('Sale')} onMouseLeave={handleMouseLeave}>
+            <div className="nav-link-title-products" id="makeup-link" >
               <p style={{ color: "red" }}>Sale</p>
             </div>
           </li>
 
-          <li className="nav-item">
-            <div className="nav-link-title-products" id="makeup-link" onMouseEnter={() => handleMouseEnter('Hair')} onMouseLeave={handleMouseLeave}>
+          <li className="nav-item" onMouseEnter={() => handleMouseEnter('Service')} onMouseLeave={handleMouseLeave}>
+            <div className="nav-link-title-products" id="makeup-link">
               Service
             </div>
           </li>
